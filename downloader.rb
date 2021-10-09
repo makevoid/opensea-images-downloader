83b3ee6130c0b663b9e09d4818043f3323bd5c60
Francesco Canessa (makevoid) <makevoid@gmail.com>
2021-10-09 20:42:46 +0100

    update downloader

--- downloader.rb
+++ downloader.rb
@@ -6,6 +6,7 @@ DATA[:offset] = 0
 
 def fetch_batch(offset: 0)
   url = API_URL % [COLLECTION_NAME, offset]
+  puts url
   res = Excon.get url
   res = JSON.parse res.body
   exit if assets_empty? res: res
@@ -25,6 +26,7 @@ def fetch_image_urls(res:)
   image_urls = []
   assets.each do |asset|
     image_url = asset.fetch "image_original_url"
+    image_url = asset.fetch "image_url" unless image_url
     image_urls.push image_url
   end
   image_urls
@@ -37,6 +39,7 @@ def download_images(image_urls:)
 end
 
 def download_image(image_url:)
+  puts image_url
   res = Excon.get image_url
   image_binary = res.body
   file_ext = File.extname image_url
