8d1c43997edf2eef4fec22d8b1b10f2f2e4c8fce
Francesco Canessa (makevoid) <makevoid@gmail.com>
2021-10-18 06:07:51 +0100

    fix

--- downloader.rb
+++ downloader.rb
@@ -43,7 +43,7 @@ def download_image(image_url:)
   res = Excon.get image_url
   image_binary = res.body
   file_ext = File.extname image_url
-  file_ext = ".png" unless file_ext
+  file_ext = ".png" if file_ext == ""
   image_num = DATA[:img_num]
   File.write "#{OUTPUT_DIR}/#{image_num}#{file_ext}", image_binary
   DATA[:img_num] += 1
